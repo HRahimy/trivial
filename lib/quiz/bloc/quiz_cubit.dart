2560e3e983e29ab4c69f39b8328f116523fd8bcf
HRahimy <rahimy.hamza@gmail.com>
2023-03-04 14:51:57 +0300

    Add some clarification to cubit parameters

--- lib/quiz/bloc/quiz_cubit.dart
+++ lib/quiz/bloc/quiz_cubit.dart
@@ -9,8 +9,15 @@ class QuizCubit extends Cubit<QuizState> {
   QuizCubit({
     required Quiz quiz,
     required List<QuizQuestion> quizQuestions,
-  }) : _quiz = quiz, _quizQuestions = quizQuestions, super(const QuizState());
+  })  : _quiz = quiz,
+        _quizQuestions = quizQuestions,
+        super(const QuizState());
 
+  /*
+  Keeping quiz and questions parameters here because their values originate from an external data source.
+  The app's functionality also will not affect these objects, they are simply read from the data source.
+  Furthermore, this implementation forces these required parameters to be available for all further functions.
+  */
   final Quiz _quiz;
   final List<QuizQuestion> _quizQuestions;
 }
