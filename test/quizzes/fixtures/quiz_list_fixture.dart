26fa5de46b8eb0223e4619a67b4288aaed515ee7
HRahimy <rahimy.hamza@gmail.com>
2023-03-09 14:44:28 +0300

    Add test to verify navigation to quiz screen is triggered upon pressing a quiz button

--- test/quizzes/fixtures/quiz_list_fixture.dart
+++ test/quizzes/fixtures/quiz_list_fixture.dart
@@ -1,24 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:repositories/repositories.dart';
+import 'package:trivial/named_routes.dart';
 import 'package:trivial/quizzes/bloc/quizzes_cubit.dart';
 import 'package:trivial/quizzes/widgets/quiz_list.dart';
+
+import '../../mocks/quiz_repository_mock.dart';
 
 class QuizListFixture extends StatelessWidget {
   const QuizListFixture({
     Key? key,
     required QuizzesCubit cubit,
+    QuizRepository? quizRepository,
+    NavigatorObserver? navigatorObserver,
   })  : _cubit = cubit,
+        _quizRepository = quizRepository,
+        _navigatorObserver = navigatorObserver,
         super(key: key);
 
   final QuizzesCubit _cubit;
+  final QuizRepository? _quizRepository;
+  final NavigatorObserver? _navigatorObserver;
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<QuizzesCubit>.value(
+    final repositoryChild = BlocProvider<QuizzesCubit>.value(
       value: _cubit,
-      child: const MaterialApp(
-        home: QuizList(),
+      child: MaterialApp(
+        home: const QuizList(),
+        routes: namedRoutes(context),
+        navigatorObservers:
+            _navigatorObserver != null ? [_navigatorObserver!] : [],
       ),
     );
+
+    return _quizRepository != null
+        ? RepositoryProvider<QuizRepository>.value(
+            value: _quizRepository!,
+            child: repositoryChild,
+          )
+        : RepositoryProvider<QuizRepository>(
+            create: (_) => MockQuizRepository(),
+            child: repositoryChild,
+          );
   }
 }
--- test/quizzes/widgets/quiz_list_screen_test.dart
+++ test/quizzes/widgets/quiz_list_screen_test.dart
@@ -3,22 +3,26 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:formz/formz.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:repositories/repositories.dart';
+import 'package:trivial/quiz/widgets/quiz_screen.dart';
 import 'package:trivial/quizzes/bloc/quizzes_cubit.dart';
 import 'package:trivial/quizzes/quizzes_keys.dart';
 
 import '../../mocks/quizzes_cubit_mock.dart';
+import '../../tests_navigator_observer.dart';
 import '../fixtures/quiz_list_fixture.dart';
 
 void main() {
   late QuizzesCubit cubit;
+  late TestsNavigatorObserver navObserver;
   const QuizzesState loadedState = QuizzesState(
     quizzes: SeedData.quizzes,
     status: FormzSubmissionStatus.success,
   );
-  
+
   group('[QuizList]', () {
     setUp(() {
       cubit = QuizzesCubitMock();
+      navObserver = TestsNavigatorObserver();
     });
 
     testWidgets('is rendered', (tester) async {
@@ -63,6 +67,32 @@ void main() {
 
       expect(finder, findsOneWidget);
       expect(widget.data, firstQuiz.name);
+    });
+
+    // Solution for testing navigation adapted from:
+    // https://harsha973.medium.com/widget-testing-pushing-a-new-page-13cd6a0bb055
+    testWidgets('quiz button triggers navigation', (tester) async {
+      bool isQuizScreenPushed = false;
+      when(() => cubit.state).thenReturn(loadedState);
+
+      await tester.pumpWidget(QuizListFixture(
+        cubit: cubit,
+        navigatorObserver: navObserver,
+      ));
+
+      navObserver.attachPushRouteObserver(
+        QuizScreen.routeName,
+        () {
+          isQuizScreenPushed = true;
+        },
+      );
+
+      await tester.tap(
+        find.byKey(QuizzesKeys.quizItemButton('${SeedData.quizzes[0].id}')),
+      );
+      await tester.pumpAndSettle();
+
+      expect(isQuizScreenPushed, true);
     });
   });
 }
