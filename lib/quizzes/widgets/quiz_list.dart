306e0fb9bc4f1bac73160b5637d242b3c9409fe1
HRahimy <rahimy.hamza@gmail.com>
2023-03-09 13:30:16 +0300

    Separate quiz list UI widget from screen widget which only deals with setting up dependencies now

---
+++ lib/quizzes/widgets/quiz_list.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:formz/formz.dart';
+import 'package:repositories/repositories.dart';
+import 'package:trivial/quiz/widgets/quiz_screen.dart';
+import 'package:trivial/quizzes/bloc/quizzes_cubit.dart';
+import 'package:trivial/quizzes/quizzes_keys.dart';
+
+class QuizList extends StatelessWidget {
+  const QuizList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: BlocBuilder<QuizzesCubit, QuizzesState>(
+            builder: (context, state) {
+              if (state.status == FormzSubmissionStatus.inProgress ||
+                  state.status == FormzSubmissionStatus.initial) {
+                return const CircularProgressIndicator();
+              } else if (state.status == FormzSubmissionStatus.failure) {
+                return Text(state.error);
+              } else {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  child: Column(
+                    key: QuizzesKeys.quizList,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: state.quizzes
+                        .map((e) => _QuizButton(
+                              key: QuizzesKeys.quizItemButton('${e.id}'),
+                              quiz: e,
+                            ))
+                        .toList(),
+                  ),
+                );
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _QuizButton extends StatelessWidget {
+  const _QuizButton({
+    Key? key,
+    required this.quiz,
+  }) : super(key: key);
+  final Quiz quiz;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => Navigator.pushNamed(
+        context,
+        QuizScreen.routeName,
+        arguments: QuizScreenArgs(quizId: quiz.id),
+      ),
+      child: Text(
+        quiz.name,
+        key: QuizzesKeys.quizItemButtonText('${quiz.id}'),
+      ),
+    );
+  }
+}
--- lib/quizzes/widgets/quiz_list_screen.dart
+++ lib/quizzes/widgets/quiz_list_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:formz/formz.dart';
 import 'package:repositories/repositories.dart';
-import 'package:trivial/quiz/widgets/quiz_screen.dart';
 import 'package:trivial/quizzes/bloc/quizzes_cubit.dart';
-import 'package:trivial/quizzes/quizzes_keys.dart';
+import 'package:trivial/quizzes/widgets/quiz_list.dart';
 
 class QuizListScreen extends StatelessWidget {
   const QuizListScreen({Key? key}) : super(key: key);
@@ -16,68 +14,7 @@ class QuizListScreen extends StatelessWidget {
       create: (_) => QuizzesCubit(
         quizRepository: RepositoryProvider.of<QuizRepository>(context),
       )..loadInitial(),
-      child: const Scaffold(
-        body: _View(),
-      ),
-    );
-  }
-}
-
-class _View extends StatelessWidget {
-  const _View({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: SingleChildScrollView(
-        child: BlocBuilder<QuizzesCubit, QuizzesState>(
-          builder: (context, state) {
-            if (state.status == FormzSubmissionStatus.inProgress ||
-                state.status == FormzSubmissionStatus.initial) {
-              return const CircularProgressIndicator();
-            } else if (state.status == FormzSubmissionStatus.failure) {
-              return Text(state.error);
-            } else {
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 32),
-                child: Column(
-                  key: QuizzesKeys.quizList,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: state.quizzes
-                      .map((e) => _QuizButton(
-                            key: QuizzesKeys.quizItemButton('${e.id}'),
-                            quiz: e,
-                          ))
-                      .toList(),
-                ),
-              );
-            }
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class _QuizButton extends StatelessWidget {
-  const _QuizButton({
-    Key? key,
-    required this.quiz,
-  }) : super(key: key);
-  final Quiz quiz;
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () => Navigator.pushNamed(
-        context,
-        QuizScreen.routeName,
-        arguments: QuizScreenArgs(quizId: quiz.id),
-      ),
-      child: Text(
-        quiz.name,
-        key: QuizzesKeys.quizItemButtonText('${quiz.id}'),
-      ),
+      child: const QuizList(),
     );
   }
 }
