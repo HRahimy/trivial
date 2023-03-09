c5bcf58b94f026a7613445f15bf0e394d034e8bb
HRahimy <rahimy.hamza@gmail.com>
2023-03-09 13:18:25 +0300

    Add quiz list tests

---
+++ TODO.md
@@ -0,0 +1,31 @@
+# ToDo
+
+## Tests
+
+### Quiz list
+
+- [x] button is rendered for a quiz
+- [x] button contains correct text
+
+### Quiz
+
+- [ ] question text exists & is correct
+- [ ] changing question updates UI with new question text
+- [ ] timer exists and is animated
+- [ ] event to deplete the question triggers when timer runs out
+- [ ] timer resets when new question is opened
+- [ ] score panel exists and renders correct score
+- [ ] options panel contains 4 buttons & renders values correctly
+- [ ] button for selected options are highlighted
+- [ ] pressing an option button triggers select event in state
+- [ ] option buttons are disabled and greyed when time is depleted
+- [ ] continue button is enabled when question is depleted or option is selected
+- [ ] pressing continue button triggers continue event in state
+
+### Quiz end screen
+
+- [ ] contains flavor text including the final score
+- [ ] contains the 'try again' button
+- [ ] 'try again' button resets the quiz
+- [ ] contains the 'goodbye' button
+- [ ] 'goodbye' button navigates to last screen
--- test/quizzes/widgets/quiz_list_screen_test.dart
+++ test/quizzes/widgets/quiz_list_screen_test.dart
@@ -9,9 +9,13 @@ import 'package:trivial/quizzes/widgets/quiz_list_screen.dart';
 import '../../mocks/quiz_repository_mock.dart';
 
 void main() {
+  late QuizRepository repository;
   group('[QuizList]', () {
+    setUp(() {
+      repository = MockQuizRepository();
+    });
+
     testWidgets('is rendered', (tester) async {
-      final repository = MockQuizRepository();
       when(() => repository.getQuizzes()).thenReturn(SeedData.quizzes);
 
       await tester.pumpWidget(
@@ -24,6 +28,50 @@ void main() {
       );
 
       expect(find.byKey(QuizzesKeys.quizList), findsOneWidget);
+    });
+
+    testWidgets('button is rendered for a quiz', (tester) async {
+      when(() => repository.getQuizzes()).thenReturn(SeedData.quizzes);
+
+      final Quiz firstQuiz = SeedData.quizzes[0];
+
+      await tester.pumpWidget(
+        RepositoryProvider<QuizRepository>.value(
+          value: repository,
+          child: const MaterialApp(
+            home: QuizListScreen(),
+          ),
+        ),
+      );
+
+      expect(
+        find.byKey(QuizzesKeys.quizItemButton('${firstQuiz.id}')),
+        findsOneWidget,
+      );
+    });
+
+    testWidgets('quiz button contains correct text', (tester) async {
+      when(() => repository.getQuizzes()).thenReturn(SeedData.quizzes);
+
+      final Quiz firstQuiz = SeedData.quizzes[0];
+
+      await tester.pumpWidget(
+        RepositoryProvider<QuizRepository>.value(
+          value: repository,
+          child: const MaterialApp(
+            home: QuizListScreen(),
+          ),
+        ),
+      );
+
+      final finder = find.descendant(
+        of: find.byKey(QuizzesKeys.quizItemButton('${firstQuiz.id}')),
+        matching: find.byKey(QuizzesKeys.quizItemButtonText('${firstQuiz.id}')),
+      );
+      final widget = tester.widget(finder) as Text;
+
+      expect(finder, findsOneWidget);
+      expect(widget.data, firstQuiz.name);
     });
   });
 }
