3c29decc5677705298a1552483900bec632d2e21
HRahimy <rahimy.hamza@gmail.com>
2023-03-21 18:12:35 +0300

    Add tests for `loadInitial()` function in quizzes cubit
    - calls `getQuizzes()` from repository
    - given successful load from repository, emits correct order of statuses
    - given load failure from repository, emits correct order of statuses

--- test/quizzes/bloc/quizzes_cubit_test.dart
+++ test/quizzes/bloc/quizzes_cubit_test.dart
@@ -1,4 +1,6 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:formz/formz.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:repositories/repositories.dart';
 import 'package:trivial/quizzes/bloc/quizzes_cubit.dart';
@@ -21,6 +23,49 @@ void main() {
   group('[QuizzesCubit]', () {
     test('initial state is `QuizzesState()`', () {
       expect(cubit.state, equals(const QuizzesState()));
+    });
+
+    group('[loadInitial()]', () {
+      blocTest(
+        'calls `getQuizzes()` from repository',
+        build: () => cubit,
+        act: (cubit) => cubit.loadInitial(),
+        verify: (contextCubit) {
+          verify(() => repository.getQuizzes()).called(1);
+        },
+      );
+      blocTest(
+        'given successful load from repository, emits correct order of statuses',
+        build: () => cubit,
+        act: (cubit) {
+          when(() => repository.getQuizzes()).thenReturn(SeedData.quizzes);
+          cubit.loadInitial();
+        },
+        expect: () => <QuizzesState>[
+          const QuizzesState(status: FormzSubmissionStatus.inProgress),
+          const QuizzesState(
+            status: FormzSubmissionStatus.success,
+            quizzes: SeedData.quizzes,
+          ),
+        ],
+      );
+
+      blocTest(
+        'given load failure from repository, emits correct order of statuses',
+        build: () => cubit,
+        act: (cubit) {
+          when(() => repository.getQuizzes())
+              .thenThrow(Exception('test error'));
+          cubit.loadInitial();
+        },
+        expect: () => <QuizzesState>[
+          const QuizzesState(status: FormzSubmissionStatus.inProgress),
+          QuizzesState(
+            status: FormzSubmissionStatus.failure,
+            error: Exception('test error').toString(),
+          ),
+        ],
+      );
     });
   });
 }
