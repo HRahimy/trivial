5ffae039e18ba31f7ccbcb4381ddde4bf5e78fba
HRahimy <rahimy.hamza@gmail.com>
2023-03-23 14:33:18 +0300

    Fix issues detected by lint job

--- lib/quiz/bloc/quiz_cubit.dart
+++ lib/quiz/bloc/quiz_cubit.dart
@@ -1,6 +1,5 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/widgets.dart';
 import 'package:formz/formz.dart';
 import 'package:repositories/repositories.dart';
 
--- lib/quiz/widgets/quiz_screen.dart
+++ lib/quiz/widgets/quiz_screen.dart
@@ -1,6 +1,5 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:repositories/repositories.dart';
 import 'package:trivial/quiz/bloc/quiz_cubit.dart';
