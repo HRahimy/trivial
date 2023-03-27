6cc2a8b934755eb752ae115d91aa78683e13db4f
HRahimy <rahimy.hamza@gmail.com>
2023-03-27 19:33:04 +0300

    Configure firebase hosting

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:repositories/repositories.dart';
 import 'package:trivial/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   final QuizRepository quizRepository = QuizRepository();
   runApp(App(
     quizRepository: quizRepository,
