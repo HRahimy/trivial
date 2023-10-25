cd163395eadac9ad8d44d2c93a78455d48917fac
HRahimy <rahimy.hamza@gmail.com>
2023-10-25 19:45:49 +0300

    refactor(Web app): Remove todo component
    Removed it in order to fix build errors caused by removal of `ngx-bootstrap`

--- src/WebStack/src/WebUI/ClientApp/src/app/app.module.ts
+++ src/WebStack/src/WebUI/ClientApp/src/app/app.module.ts
@@ -3,15 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-import { ModalModule } from 'ngx-bootstrap/modal';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { TodoComponent } from './todo/todo.component';
 import { TokenComponent } from './token/token.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 
@@ -26,7 +23,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    TodoComponent,
     TokenComponent,
   ],
   imports: [
@@ -37,7 +33,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSidenavModule,
-    ModalModule.forRoot(),
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true },
