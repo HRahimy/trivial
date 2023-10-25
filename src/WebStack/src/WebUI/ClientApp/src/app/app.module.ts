1600519bcf5fd9383a0cb0cc56bd7c8bcde257fb
HRahimy <rahimy.hamza@gmail.com>
2023-10-25 21:19:51 +0300

    enh(Web app): Add nav list to sidenav

--- src/WebStack/src/WebUI/ClientApp/src/app/app.module.ts
+++ src/WebStack/src/WebUI/ClientApp/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { TokenComponent } from './token/token.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
@@ -33,6 +36,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSidenavModule,
+    MatListModule,
+    MatDividerModule,
+    MatToolbarModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true },
--- src/WebStack/src/WebUI/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ src/WebStack/src/WebUI/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,18 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  ActivatedRoute,
+  ActivatedRouteSnapshot,
+  NavigationEnd,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+
+import { Subscription, filter } from 'rxjs';
+
+export enum AppLinkType {
+  Route = 'route',
+  ExternalLink = 'externalLink',
+}
+
+export interface AppLink {
+  route: string;
+  name: string;
+  type: AppLinkType;
+  isActive: boolean;
+}
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.scss']
+  styleUrls: ['./nav-menu.component.scss'],
 })
-export class NavMenuComponent {
-  isExpanded = false;
+export class NavMenuComponent implements OnDestroy {
+  linkTypes = AppLinkType;
+  links: AppLink[] = [
+    {
+      route: '/',
+      name: 'Home',
+      type: AppLinkType.Route,
+      isActive: false,
+    },
+    {
+      route: '/counter',
+      name: 'Counter',
+      type: AppLinkType.Route,
+      isActive: false,
+    },
+    {
+      route: '/fetch-data',
+      name: 'Fetch data',
+      type: AppLinkType.Route,
+      isActive: false,
+    },
+    {
+      route: '/api',
+      name: 'API',
+      type: AppLinkType.ExternalLink,
+      isActive: false,
+    },
+  ];
 
-  collapse() {
-    this.isExpanded = false;
+  routerSub: Subscription;
+
+  constructor(private router: Router) {
+    this.routerSub = this.router.events
+      .pipe(filter((e) => e instanceof NavigationEnd))
+      .subscribe({
+        next: (value: NavigationEnd) => {
+          this.links = this.links.map((e) => {
+            return {
+              ...e,
+              isActive: value.url === e.route,
+            };
+          });
+        },
+      });
   }
 
-  toggle() {
-    this.isExpanded = !this.isExpanded;
+  ngOnDestroy(): void {
+    this.routerSub.unsubscribe();
   }
 }
