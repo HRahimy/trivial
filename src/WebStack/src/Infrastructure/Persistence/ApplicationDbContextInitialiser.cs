cf01875ba9bf84277b0d0b1fddf4f9ca05e31997
Hamza Yazar <rahimy.hamza@gmail.com>
2023-11-01 16:47:37 +0300

    enh(Backend): Add seed data for quizzes

--- src/WebStack/src/Infrastructure/Persistence/ApplicationDbContextInitialiser.cs
+++ src/WebStack/src/Infrastructure/Persistence/ApplicationDbContextInitialiser.cs
@@ -3,6 +3,7 @@ using Trivial.Infrastructure.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Trivial.Domain.Enums;
 
 namespace Trivial.Infrastructure.Persistence;
 
@@ -68,7 +69,7 @@ public class ApplicationDbContextInitialiser
             await _userManager.CreateAsync(administrator, "Administrator1!");
             if (!string.IsNullOrWhiteSpace(administratorRole.Name))
             {
-                await _userManager.AddToRolesAsync(administrator, new [] { administratorRole.Name });
+                await _userManager.AddToRolesAsync(administrator, new[] { administratorRole.Name });
             }
         }
 
@@ -86,6 +87,70 @@ public class ApplicationDbContextInitialiser
                     new TodoItem { Title = "Realise you've already done two things on the list! 🤯"},
                     new TodoItem { Title = "Reward yourself with a nice, long nap 🏆" },
                 }
+            });
+
+            await _context.SaveChangesAsync();
+        }
+
+        // Quizzes
+        if (!_context.Quizzes.Any())
+        {
+            _context.Quizzes.Add(new Quiz
+            {
+                Name = "World of Warcraft",
+                Description = "Test your knowledge of the World of Warcraft lore. For beginners to the universe.",
+                Duration = new TimeSpan(0, 15, 30),
+                Difficulty = QuizDifficulty.Normal,
+                Questions =
+                {
+                    new QuizQuestion
+                    {
+                        Question = "Who was the High Chieftain of the Tauren before Baine Bloodhoof",
+                        SequenceIndex = 1,
+                        Options =
+                        {
+                            new QuestionOption { Value = QuestionOptionIndex.A, Description = "Cairne Bloodhoof", IsAnswer = true },
+                            new QuestionOption { Value = QuestionOptionIndex.B, Description = "Varok Saurfang", IsAnswer = false },
+                            new QuestionOption { Value = QuestionOptionIndex.C, Description = "Tamalaa Bloodhoof", IsAnswer = false },
+                            new QuestionOption { Value = QuestionOptionIndex.D, Description = "Elder Bloodhoof", IsAnswer = false },
+                        }
+                    },
+                    new QuizQuestion
+                    {
+                        Question = "Which of these creatures are native to Elwynn Forest?",
+                        SequenceIndex = 2,
+                        Options =
+                        {
+                            new QuestionOption { Value = QuestionOptionIndex.A, Description = "Quillboar", IsAnswer = false },
+                            new QuestionOption { Value = QuestionOptionIndex.B, Description = "Murlocs", IsAnswer = true },
+                            new QuestionOption { Value = QuestionOptionIndex.C, Description = "Tauren", IsAnswer = false },
+                            new QuestionOption { Value = QuestionOptionIndex.D, Description = "Dreadlords", IsAnswer = false },
+                        }
+                    },
+                    new QuizQuestion
+                    {
+                        Question = "Which of these creatures are native to Elwynn Forest?",
+                        SequenceIndex = 2,
+                        Options =
+                        {
+                            new QuestionOption { Value = QuestionOptionIndex.A, Description = "Quillboar", IsAnswer = false },
+                            new QuestionOption { Value = QuestionOptionIndex.B, Description = "Murlocs", IsAnswer = true },
+                            new QuestionOption { Value = QuestionOptionIndex.C, Description = "Tauren", IsAnswer = false },
+                            new QuestionOption { Value = QuestionOptionIndex.D, Description = "Dreadlords", IsAnswer = false },
+                        }
+                    },
+                }
+            });
+            _context.Quizzes.Add(new Quiz
+            {
+                Name = "Elder Scrolls",
+                Description = "Test your knowledge of the Elder Scrolls lore. For beginners to the universe.",
+                Duration = new TimeSpan(0, 15, 30),
+                Difficulty = QuizDifficulty.Normal,
+                //Questions =
+                //{
+                //    new QuizQuestion{Question = ""}
+                //}
             });
 
             await _context.SaveChangesAsync();
